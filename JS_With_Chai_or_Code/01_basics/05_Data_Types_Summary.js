e379e509f4184f55d853d0d18bae550d67f8ed03
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-01 20:48:32 +0530

    complete summary of all datatypes

---
+++ JS_With_Chai_or_Code/01_basics/05_Data_Types_Summary.js
@@ -0,0 +1,36 @@
+//  Primitive
+// 7 types : String, Number, Boolean, Null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console. log(id === anotherId);
+
+const bigNumber = 23345434323245467897654334243242n;     // undefined 
+
+
+// Refrence type (Non primitive )
+// Array, Objects, Functions
+
+const heros = ["aditya", "harshit", "Deepak"]
+
+let obj = {
+  name : "Aditya",
+  age : 22
+}
+
+
+const myFun  = function(){};
+
+
+
+
+
