42f475650f777eb041cd374ec325681b59788a88
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-01 20:46:41 +0530

    complete comparison

---
+++ JS_With_Chai_or_Code/01_basics/04_Comparison.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 = 1);
+// console.log(2 != 1);
+
+
+
+// avoid these type of comparison
+console.log("2" > 1);
+console.log("02" > 1);        // it don't alow 
+
+console.log(null > 0);         // f
+console.log(null == 0);       //f
+console.log(null >= 0);      //t
+
+
+console.log(undefined = 0);      
+console.log(undefined > 0);      
+console.log(undefined < 0);      
+
+
+
+
+//  ====     Strictly operator
+console.log("2"  === 2);
