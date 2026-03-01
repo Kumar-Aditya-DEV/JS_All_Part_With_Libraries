0dff59be71aa5b1d04aba3e9c09bd91e990e4286
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-01 20:45:06 +0530

    complete dataTypes

---
+++ JS_With_Chai_or_Code/01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3);     // we are using node js not browser
+
+console.log(3 + 3);
+console.log("Hitesh");
+
+
+let name = "aditya"
+let age = 11;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof age);
+console.log(typeof null);           //  -> object
+console.log(typeof undefined);        // -> undefined
