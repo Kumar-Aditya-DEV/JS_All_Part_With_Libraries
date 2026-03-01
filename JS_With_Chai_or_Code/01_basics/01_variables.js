41839636fd3eb9c60e8ab68947007416d74f353e
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-01 20:44:06 +0530

    variables basics complete

---
+++ JS_With_Chai_or_Code/01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 14453;
+let accountEmail  = "drffsfs@gmail.com"
+var accountPassword = "12344"
+accountCity = "Jaipur";
+let accountState;
+
+// accountId  = 2; 
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
