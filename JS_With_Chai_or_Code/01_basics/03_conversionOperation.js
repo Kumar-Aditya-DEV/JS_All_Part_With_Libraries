8e9fc7bbefd38eb8550b6df4cc3e083a6ef53b22
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-01 20:45:49 +0530

    complete conversion of datatypes

---
+++ JS_With_Chai_or_Code/01_basics/03_conversionOperation.js
@@ -0,0 +1,81 @@
+let score = "33abc";
+
+console.log(typeof score);
+console.log(typeof (score));
+console.log();
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log();
+
+
+// "3" = > 3
+// "3asd" = > NaN
+// true  => 1; false => 0;
+
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// " " => false
+// "hitesh" => true
+
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console. log(stringNumber);
+console. log(typeof stringNumber);
+
+
+
+
+
+
+// ************************ Operations ************************
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2%2);
+// console.log(2**2);
+
+
+let str1 = "Hellow"
+let str2 = " Aditya"
+
+let str3 = str1 + str2;
+console.log(str3);
+
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((1 + 2) * 5 % 3);
+
+
+
+// console.log(+true);         // 1 dega
+// console.log(+"");         // 0 dega  -> no sense
+
+
+let num1, num2, num3
+num1 = num2 = num3  = 2 + 2
+
+
+let gameCounter = 100
+// gameCounter++;
+// ++gameCounter;
+console.log(gameCounter);
+
