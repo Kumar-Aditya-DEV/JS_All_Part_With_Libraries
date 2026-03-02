120a2c07444f6525d1c42325600372f3a6c78edd
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-03-02 21:50:03 +0530

    learn string

---
+++ JS_With_Chai_or_Code/01_basics/05_strings.js
@@ -0,0 +1,27 @@
+const name  = "Aditya "
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hellow my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hiteshhc')
+console.log(gameName);
+console.log(gameName[0]);
+console.log(gameName[1]);
+console.log(gameName.__proto__);        // ye empty object dikh raha hai but hai nii
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());      // original value change ni hui hai
+console.log(gameName.charAt(2));    // kon si posn pr hai
+console.log(gameName.indexOf('t'));   // ye bataiega t kon s numer pr hai
+console.log(gameName);
+console.log(gameName);
+console.log(gameName);
+console.log(gameName);
+console.log(gameName); 
+
+
+
+
